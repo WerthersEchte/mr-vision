73958a10e0c143299eaadc4055f5242aec8da588
grace <grace@robotic-lab.fh-kiel.de>
2015-01-13 17:06:43 +0100

    gridlines

--- src/gui/cameragui.cpp
+++ src/gui/cameragui.cpp
@@ -3,6 +3,7 @@
 
 #include <QThread>
 #include <QtConcurrent/QtConcurrent>
+#include <QPainter>
 
 namespace mrvision {
 
@@ -46,6 +47,9 @@ void CameraGui::streamLoop(){
 
     const uchar *qImageBuffer;
     cv::Mat *mat;
+    QPixmap vPixmap;
+
+    QPen LinePenRed(Qt::red,1), LinePenGreen(Qt::green,1);
 
     QVector<QRgb> vColorTable;
     for (int i=0; i<256; i++){
@@ -61,7 +65,37 @@ void CameraGui::streamLoop(){
         QImage img(qImageBuffer, mat->cols, mat->rows, mat->step, QImage::Format_Indexed8);
         img.setColorTable(vColorTable);
 
-        emit newPicture(QPixmap::fromImage(img));
+        vPixmap = QPixmap::fromImage(img);
+
+        if( mUi->cBGridlines->isChecked() ){
+
+            QPainter painter(&vPixmap);
+            painter.setPen(LinePenRed);
+
+            for( int i = 10; i < vPixmap.height(); i = i + 10){
+                if( !(i%50) ){
+                    painter.setPen(LinePenGreen);
+                    painter.drawLine(0,i,vPixmap.width(),i);
+                    painter.setPen(LinePenRed);
+                } else {
+                    painter.drawLine(0,i,vPixmap.width(),i);
+                }
+            }
+            for( int i = 10; i < vPixmap.width(); i = i + 10){
+                if( !(i%50) ){
+                    painter.setPen(LinePenGreen);
+                    painter.drawLine(i,0,i,vPixmap.height());
+                    painter.setPen(LinePenRed);
+                } else {
+                    painter.drawLine(i,0,i,vPixmap.height());
+                }
+            }
+
+            painter.end();
+
+        }
+
+        emit newPicture(vPixmap);
 
     }
 
